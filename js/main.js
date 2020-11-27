9db2fe518417e532a2e41c4723774d3b6a35caf5
InetaVei <74532995+InetaVei@users.noreply.github.com>
2020-11-27 23:02:30 +0200

    learning js

---
+++ js/main.js
@@ -0,0 +1,86 @@
+/* 
+K I N T A M U J U    I N I C I J A V I M A S */
+
+
+const skaicius1 = 123;
+console.log(skaicius1); 
+
+const numeris = 6;
+console.log(numeris);
+
+const eilesNumeris = 785;
+console.log(eilesNumeris);
+
+const visiIsvardinti = skaicius1 + numeris + eilesNumeris;
+console.log(visiIsvardinti);
+
+/* 2 uzduotis */
+
+
+const vardas = 'Elzbieta';
+console.log(vardas);
+
+const veiksmas = 'megsta';
+console.log(veiksmas);
+
+const italiskasPatiekalas = 'pica';
+console.log(italiskasPatiekalas);
+
+const sakinys = vardas + ' ' + veiksmas + ' ' + italiskasPatiekalas;
+console.log(sakinys);
+
+const klaus = 'Kaip';
+const kreip = 'tu';
+const live = 'gyveni';
+const klausimas = live + ' ' + kreip + ',' + klaus;
+console.log(klausimas);
+
+
+/* 3 uzduotis */
+
+const sarasas = [1, 2, 3, 4, 5];
+console.log(sarasas);
+
+const list = [11, 22, 33, 44, 55];
+console.log(list);
+
+const eiles = [12, 13, 14, 15, 16];
+console.log(eiles);
+
+/* 4 uzduotis
+Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console */
+
+const meniu = ['[pica, blynai, kose, salotos, pyragas'];
+console.log(meniu);
+
+const darboDienos = ['pirmadienis, antradienis, treciadienis, ketvirtadienis, penktadienis'];
+console.log(darboDienos);
+
+const spalvos = ['balta, raudona, geltona, melyna, alyvine'];
+console.log(spalvos);
+
+
+/* V E I K S M A I   S U   K I N T A M A I S I A I S */
+
+
+const numb1 = 10;
+const numb2 = 20;
+const numb3 = 30;
+
+const suma = numb1 + numb2 + numb3;
+console.log(suma);
+
+const no1 = 11;
+const no2 = 22;
+const no3 = 33;
+
+const no4 = no1 + no2 + no3;
+console.log(no4); 
+
+
+const matem = [1, 2, 3, 4, 5];
+const veiksm = matem[0] - matem[1] + matem[2] - matem[3] + matem[4];
+console.log(veiksm);
+
+
